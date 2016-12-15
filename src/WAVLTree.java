f5cc0e6e39b57a8fa01fdb85eea9bfa4f72151ea
Ofir Feffer <ofir.feffer@gmail.com>
2016-12-16 00:43:00 +0200

    lots of deletion bugs

--- src/WAVLTree.java
+++ src/WAVLTree.java
@@ -1,7 +1,9 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 /**
  * WAVLTree
@@ -185,18 +187,20 @@ public class WAVLTree {
 			} else if (diff == RankDiff.D0_2) {
 				if (prev == RankDiff.D1_2) {
 					rotateRight(node, false);
+					counter++;
 				} else {
 					doubleRotateLeftRight(node, false);
+					counter += 2;
 				}
-				counter++;
 				return counter;
 			} else if (diff == RankDiff.D2_0) {
 				if (prev == RankDiff.D2_1) {
 					rotateLeft(node, false);
+					counter++;
 				} else {
 					doubleRotateRightLeft(node, false);
+					counter += 2;
 				}
-				counter++;
 				return counter;
 			} else if (diff == RankDiff.D1_1) {
 				return counter;
@@ -256,26 +260,41 @@ public class WAVLTree {
 	 */
 	public int delete(WAVLNode parent, WAVLNode child, boolean isLeftChild) {
 		NodeType type = NodeType.of(child);
+		int res;
+
 		if (type == NodeType.LEAF) { // case 1: the node to be deleted has no
 										// children.
 			deleteLeafNode(parent, isLeftChild);
+
+			if (parent == null) {
+				return 0;
+			} else if (NodeType.of(parent) == NodeType.LEAF) {
+				res = balanceAfterDeletion(parent);
+			} else if (isLeftChild) {
+				res = balanceAfterDeletion(parent.rightChild);
+			} else {
+				res = balanceAfterDeletion(parent.leftChild);
+			}
 
 		} else if (type == NodeType.LEFT_CHILD_ONLY) { // case 2.1: the node to
 														// be deleted has only a
 														// left child
 			deleteNodeWithOneChild(parent, child.leftChild, isLeftChild);
+			res = balanceAfterDeletion(child.leftChild);
 
 		} else if (type == NodeType.RIGHT_CHILD_ONLY) { // case 2.2: the node to
 														// be deleted has only a
 														// right child
 			deleteNodeWithOneChild(parent, child.rightChild, isLeftChild);
+			res = balanceAfterDeletion(child.rightChild);
 
 		} else { // case 3: the node to be deleted has 2 children
-			deleteNodeWithTwoChildren(parent, child, isLeftChild);
+			WAVLNode n = deleteNodeWithTwoChildren(parent, child, isLeftChild);
+			res = balanceAfterDeletion(n);
 		}
 
 		size--;
-		return 0;
+		return res;
 	}
 
 	/**
@@ -329,7 +348,7 @@ public class WAVLTree {
 	 *            - should be set to true if the node to be deleted is a left
 	 *            child of its parent
 	 */
-	private void deleteNodeWithTwoChildren(WAVLNode parent, WAVLNode child, boolean isLeftChild) {
+	private WAVLNode deleteNodeWithTwoChildren(WAVLNode parent, WAVLNode child, boolean isLeftChild) {
 		// find the successor of the node to be deleted
 		ParentChildPair pair = minNode(child.rightChild);
 		WAVLNode successor = pair.child;
@@ -351,11 +370,99 @@ public class WAVLTree {
 
 		// now we assign our successor its new children - those of the node we
 		// want to delete
+		successor.rank = child.rank;
 		successor.setLeftChild(child.leftChild);
 		if (successorParent != null) { // to prevent a node from being its own
 										// child
 			successor.setRightChild(child.rightChild);
 		}
+
+		if (successorParent != null) {
+			return successorParent;
+		}
+		return successor;
+	}
+
+	private int balanceAfterDeletion(WAVLNode node) {
+		if (node == null) {
+			return 0;
+		}
+
+		RankDiff prev = RankDiff.D1_1;
+		int counter = 0;
+
+		while (node != null) {
+			RankDiff diff = RankDiff.of(node);
+			try {
+				if (diff == RankDiff.D2_2 && NodeType.of(node) == NodeType.LEAF) {
+					node.rank--;
+					counter++;
+					prev = RankDiff.D1_1;
+				} else if (diff == RankDiff.D3_2) {
+					node.rank--;
+					counter++;
+					prev = RankDiff.D2_1;
+				} else if (diff == RankDiff.D2_3) {
+					node.rank--;
+					counter++;
+					prev = RankDiff.D1_2;
+				} else if (diff == RankDiff.D3_1) {
+					if (prev == RankDiff.D2_2) {
+						node.rank--;
+						node.rightChild.rank--;
+						counter += 2;
+						prev = RankDiff.D2_1;
+					} else if (prev == RankDiff.D1_1 || prev == RankDiff.D2_1) {
+						rotateLeft(node, true);
+						counter++;
+						return counter;
+					} else {
+						doubleRotateRightLeft(node, true);
+						counter += 2;
+						return counter;
+					}
+				} else if (diff == RankDiff.D1_3) {
+					if (prev == RankDiff.D2_2) {
+						node.rank--;
+						node.leftChild.rank--;
+						counter += 2;
+						prev = RankDiff.D1_2;
+					} else if (prev == RankDiff.D1_1 || prev == RankDiff.D1_2) {
+						rotateRight(node, true);
+						counter++;
+						return counter;
+					} else {
+						doubleRotateLeftRight(node, true);
+						counter += 2;
+						return counter;
+					}
+				} else if (diff == RankDiff.D0_1) {
+					node.rank++;
+					counter++;
+					prev = RankDiff.D1_2;
+				} else if (diff == RankDiff.D1_0) {
+					node.rank++;
+					counter++;
+					prev = RankDiff.D2_1;
+				} else {
+					prev = diff;
+				}
+
+				node = node.parent;
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(diff);
+				System.out.println(prev);
+				System.out.println(node.rightChild);
+				System.out.println(node.leftChild);
+				System.out.println(node.rightChild.rank);
+				System.out.println(node.leftChild.rank);
+				System.out.println(node.rank);
+				System.exit(1);
+			}
+		}
+
+		return counter;
 	}
 
 	/**
@@ -497,8 +604,8 @@ public class WAVLTree {
 		node.rank--;
 		if (afterDeletion) {
 			k.rank++;
-			if (k.rightChild != null && NodeType.of(k.rightChild) == NodeType.LEAF) {
-				k.rightChild.rank--;
+			if (RankDiff.of(node) == RankDiff.D2_2 && NodeType.of(node) == NodeType.LEAF) {
+				node.rank--;
 			}
 		}
 	}
@@ -524,8 +631,8 @@ public class WAVLTree {
 		node.rank--;
 		if (afterDeletion) {
 			k.rank++;
-			if (k.leftChild != null && NodeType.of(k.leftChild) == NodeType.LEAF) {
-				k.leftChild.rank--;
+			if (RankDiff.of(node) == RankDiff.D2_2 && NodeType.of(node) == NodeType.LEAF) {
+				node.rank--;
 			}
 		}
 	}
@@ -546,7 +653,7 @@ public class WAVLTree {
 		rotateLeft(node, afterDeletion);
 
 		if (afterDeletion) {
-			node.rank--;
+			// node.rank--;
 		} else {
 			node.parent.rank++;
 		}
@@ -607,30 +714,39 @@ public class WAVLTree {
 		 * System.out.println("i2"); tree.insert(2, "Bla22"); tree.insert(1,
 		 * "Bla1"); tree.insert(8, "Bla8");
 		 */
-		for (int i = 1; i <= 10; i++) {
-			tree.insert(i, String.valueOf(i));
-		}
+		/*
+		 * for (int i = 1; i <= 10; i++) { tree.insert(i, String.valueOf(i)); }
+		 * 
+		 * TreePrint tp = tree.new TreePrint(); tp.printNode(tree.root);
+		 * 
+		 * System.out.printf("size: %d%n", tree.size()); System.out.printf(
+		 * "max: %s%n", tree.max()); System.out.printf("min: %s%n", tree.min());
+		 * System.out.println(Arrays.toString(tree.keysToArray()));
+		 * System.out.println(Arrays.toString(tree.infoToArray()));
+		 * System.out.printf("info of 2: %s%n", tree.search(2));
+		 * System.out.printf("info of 1: %s%n", tree.search(1));
+		 * System.out.printf("info of 8: %s%n", tree.search(8));
+		 * System.out.printf("info of 10: %s%n", tree.search(10));
+		 * 
+		 * tree.delete(5); tp.printNode(tree.root); tree.delete(6);
+		 * tp.printNode(tree.root); tree.delete(7); tp.printNode(tree.root); //
+		 * tree.delete(8); tree.delete(2); tp.printNode(tree.root); //
+		 * tree.delete(1);
+		 * System.out.println(Arrays.toString(tree.keysToArray()));
+		 * tree.delete(4); tp.printNode(tree.root);
+		 */
+		tree.run();
 
+		tree.insert(2, null);
+		tree.insert(1, null);
+		tree.insert(4, null);
+		tree.insert(3, null);
 		TreePrint tp = tree.new TreePrint();
 		tp.printNode(tree.root);
+		tree.delete(1);
 
-		System.out.printf("size: %d%n", tree.size());
-		System.out.printf("max: %s%n", tree.max());
-		System.out.printf("min: %s%n", tree.min());
-		System.out.println(Arrays.toString(tree.keysToArray()));
-		System.out.println(Arrays.toString(tree.infoToArray()));
-		System.out.printf("info of 2: %s%n", tree.search(2));
-		System.out.printf("info of 1: %s%n", tree.search(1));
-		System.out.printf("info of 8: %s%n", tree.search(8));
-		System.out.printf("info of 10: %s%n", tree.search(10));
+		tp.printNode(tree.root);
 
-		tree.delete(5);
-		tree.delete(6);
-		tree.delete(7);
-		// tree.delete(8);
-		tree.delete(2);
-		// tree.delete(1);
-		System.out.println(Arrays.toString(tree.keysToArray()));
 	}
 
 	class TreePrint {
@@ -656,7 +772,7 @@ public class WAVLTree {
 			List<WAVLTree.WAVLNode> newNodes = new ArrayList<WAVLTree.WAVLNode>();
 			for (WAVLTree.WAVLNode node : list) {
 				if (node != null) {
-					System.out.print(node.key);
+					System.out.print(node.rank);
 					newNodes.add(node.leftChild);
 					newNodes.add(node.rightChild);
 				} else {
@@ -720,4 +836,304 @@ public class WAVLTree {
 		}
 
 	}
+
+	public ArrayList<WAVLTree.WAVLNode> leafs = new ArrayList<WAVLTree.WAVLNode>();
+	public WAVLTree generatedTree;
+	public int totalInsertRebalances;
+	public int totalDeleteRebalances;
+	public int maxNumberOfRebalancesInInsert;
+	public int maxNumberOfRebalancesInDelete;
+	public ArrayList<Integer> keysInTree = new ArrayList<>();
+
+	public void treeGenerator(int size, int IntsLimit) {
+		Random random = new Random();
+		generatedTree = new WAVLTree();
+		int numberOfRebalances = 0;
+		int randomNumber;
+		System.out.println("Start inserting elements!");
+		for (int i = 0; i < size; i++) {
+			randomNumber = random.nextInt(IntsLimit);
+			if (keysInTree.contains(randomNumber)) {
+				i--;
+				continue;
+			}
+			keysInTree.add(randomNumber);
+			numberOfRebalances = generatedTree.insert(randomNumber, Integer.toString(randomNumber));
+			totalInsertRebalances = numberOfRebalances > 0 ? totalInsertRebalances + numberOfRebalances
+					: totalInsertRebalances;
+			if (maxNumberOfRebalancesInInsert < numberOfRebalances)
+				maxNumberOfRebalancesInInsert = numberOfRebalances;
+			if ((i == ((int) size * 3 / 4)) || (i == (int) size / 2)) {
+				System.out.println(Integer.toString(i)
+						+ " elements were inserted to the tree\n##Testing ranks and ranks differences");
+				RankTest(0); // Calls rank test method (Insert version)
+			}
+		}
+		System.out.println(
+				"all elements were inserted to the tree\n##Testing ranks and ranks differences one last time!! XDDDDD");
+		RankTest(0); // Calls rank test method (Insert version)
+
+		System.out.println("done inserting nodes!..");
+		// Sort the key's list
+		keysInTree.sort(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1 - o2;
+			}
+
+		});
+	}
+
+	public void initialize() {
+		this.generatedTree = null;
+		this.keysInTree = new ArrayList<>();
+		this.leafs = new ArrayList<>();
+		this.totalDeleteRebalances = 0;
+		this.totalInsertRebalances = 0;
+	}
+
+	public void testSize() {
+		if (generatedTree.size() != this.keysInTree.size())
+			System.err.println("Problem with size method! :/");
+		else
+			System.out.println("Size method works!");
+	}
+
+	public void testEmpty() {
+		if (!generatedTree.empty())
+			System.err.println("Problem with empty method! :/");
+		else
+			System.out.println("Empty method works!");
+	}
+
+	public void treeDelete() {
+		int RebalancesInSingleDelete = 0;
+		System.out.println("Start deleting nodes (in keys order)!");
+		int firstTestItem = keysInTree.get((int) (keysInTree.size() / 2)); // Test
+																			// ranks
+																			// and
+																			// rank-diff
+																			// after
+																			// deleting
+																			// 1/2
+																			// of
+																			// the
+																			// tree
+		int secondTestItem = keysInTree.get((int) (3 * keysInTree.size() / 4)); // Test
+																				// ranks
+																				// and
+																				// rank-diff
+																				// after
+																				// deleting
+																				// 3/4
+																				// of
+																				// the
+																				// tree
+		for (int min : keysInTree) {
+			RebalancesInSingleDelete = generatedTree.delete(min);
+			totalDeleteRebalances = RebalancesInSingleDelete > 0 ? RebalancesInSingleDelete + totalDeleteRebalances
+					: totalDeleteRebalances;
+			if (maxNumberOfRebalancesInDelete < RebalancesInSingleDelete)
+				maxNumberOfRebalancesInDelete = RebalancesInSingleDelete;
+			if (min == firstTestItem) {
+				System.out.println(((int) (keysInTree.size() / 2))
+						+ " elements were deleted from the tree\n##Testing ranks and ranks differences");
+				RankTest(1); // Calls rank test method (regular version)
+			} else if (min == secondTestItem) {
+				System.out.println(((int) (3 * keysInTree.size() / 4))
+						+ " elements were deleted from the tree\n##Testing ranks and ranks differences");
+				RankTest(1); // Calls rank test method (regular version)
+			}
+		}
+		System.out.println("Done deleting nodes.. ;)");
+	}
+
+	public void testExistanceOfElements() {
+		if (generatedTree.size != keysInTree.size())
+			System.err.println("There is some problem with your insert method :'(");
+		for (int key : keysInTree) {
+			String val = generatedTree.search(key);
+			if (val == null || !val.equals(Integer.toString(key))) {
+				System.err.println("There is some problem with your insert method :'(");
+				System.exit(1);
+			}
+		}
+		System.out.println("All elements are in the tree! Good job.. :)");
+	}
+
+	public void createLeafList() {
+		leafs = new ArrayList<>();
+		/*
+		 * int[] array = generatedTree.keysToArray(); for (int i : array) {
+		 * WAVLTree.WAVLNode node =
+		 * generatedTree.TreePosition(generatedTree.root, i); if (node.key == i
+		 * && node.leftChild == null && node.rightChild == null)
+		 * leafs.add(node); }
+		 */
+		shtut(leafs, generatedTree.root);
+	}
+
+	public void shtut(ArrayList<WAVLNode> shtut, WAVLNode node) {
+		if (node == null) {
+			return;
+		}
+		if (NodeType.of(node) == NodeType.LEAF) {
+			shtut.add(node);
+		} else {
+			shtut(shtut, node.leftChild);
+			shtut(shtut, node.rightChild);
+		}
+	}
+
+	public void RankTest(int i) {
+		createLeafList();
+		boolean RanksTest = true;
+		boolean RankDifferenceTest = true;
+		if (i == 1) {
+			for (WAVLTree.WAVLNode node : leafs) {
+				RanksTest = RanksTest && RanksTest(node);
+				RankDifferenceTest = RankDifferenceTest && RankDifferenceTest(node);
+			}
+		} else if (i == 0) {
+			for (WAVLTree.WAVLNode node : leafs) {
+				RanksTest = RanksTest && RanksTestInsertOnly(node);
+				RankDifferenceTest = RankDifferenceTest && RankDifferenceTest(node);
+			}
+
+		}
+		if (!RanksTest)
+			System.err.println("Problem with ranks... :/");
+		else
+			System.out.println("Ranks are fine so far! ;)");
+		if (!RankDifferenceTest)
+			System.err.println("Problem with rank difference... :/");
+		else
+			System.out.println("Rank diff 'r great!!! XD");
+	}
+
+	public boolean RankDifferenceTest(WAVLTree.WAVLNode node) {
+		while (node != null) {
+			int leftDiff = node.leftChild != null ? node.rank - node.leftChild.rank : node.rank + 1;
+			int rightDiff = node.rightChild != null ? node.rank - node.rightChild.rank : node.rank + 1;
+			if ((rightDiff != 1 && rightDiff != 2) && (leftDiff != 1 && leftDiff != 2)) {
+				return false;
+			}
+			node = node.parent;
+		}
+		return true;
+	}
+
+	// Ranks test, regular version!
+	// In regular version the method check whether each node's rank is
+	// max(node.left,node.right)+1 or max(node.left,node.right)+2
+
+	public boolean RanksTest(WAVLTree.WAVLNode node) {
+		while (node != null) {
+			int leftChildsRank = node.leftChild != null ? node.leftChild.rank : -1;
+			int rightChildsRank = node.rightChild != null ? node.rightChild.rank : -1;
+			if ((node.rank != Math.max(rightChildsRank, leftChildsRank) + 1)
+					&& (node.rank != Math.max(rightChildsRank, leftChildsRank) + 2))
+				return false;
+			node = node.parent;
+		}
+		return true;
+	}
+
+	// Ranks test, insert-only version!
+	// In insert only version the method check whether each node's rank is
+	// max(node.left,node.right)+1
+	public boolean RanksTestInsertOnly(WAVLTree.WAVLNode node) {
+		while (node != null) {
+			int leftChildsRank = node.leftChild != null ? node.leftChild.rank : -1;
+			int rightChildsRank = node.rightChild != null ? node.rightChild.rank : -1;
+			if (node.rank != Math.max(rightChildsRank, leftChildsRank) + 1)
+				return false;
+			node = node.parent;
+		}
+		return true;
+	}
+
+	public void testMinAndMax() {
+		if (!this.generatedTree.max().equals(Integer.toString(this.keysInTree.get(this.keysInTree.size() - 1))))
+			System.err.println("Problem with max method.. :/");
+		if (!this.generatedTree.min().equals(Integer.toString(this.keysInTree.get(0))))
+			System.err.println("Problem with min method.. :/");
+		System.out.println("Min&Max methods work.. ^_^");
+	}
+
+	public void testKeysToArrayAndInfoToArray() {
+		String[] infoToArray = generatedTree.infoToArray();
+		int[] keysToArray = generatedTree.keysToArray();
+		int cnt = 0;
+		for (int i : keysInTree) {
+			if (!infoToArray[cnt].equals(Integer.toString(i))) {
+				System.err.println("Problem with infoToArray method!! :S");
+				System.exit(1);
+			}
+			if (keysToArray[cnt] != i) {
+				System.err.println("Problem with keysToArrat method!! :S");
+				System.exit(1);
+			}
+			cnt++;
+		}
+		System.out.println("infoToArray & keysToArray methods work.");
+
+	}
+
+	public void printTree() {
+		TreePrint printer = new TreePrint();
+		printer.printNode(generatedTree.root);
+	}
+
+	public void printInfo() {
+		DecimalFormat df = new DecimalFormat();
+		df.setMaximumFractionDigits(2);
+		System.out.println("********************************************************");
+		System.out.println("Number of inserts: " + keysInTree.size());
+		if (maxNumberOfRebalancesInInsert > 2 * Math.log10(keysInTree.size()) / Math.log10(2)) {
+			System.err.println("2*log(" + keysInTree.size() + ")= "
+					+ df.format(2 * (Math.log10(keysInTree.size()) / Math.log10(2)))
+					+ "\nWorst case in insert is greater than log(n)!\nCheck your rebalance's methods!");
+		}
+		System.out.println("Number of rebalancing operations in all inserts: " + totalInsertRebalances);
+		System.out.println("Worst Case rebalancing operations in single insert: " + maxNumberOfRebalancesInInsert);
+		System.out.println("Average rebalancing operations in insert: "
+				+ df.format((double) totalInsertRebalances / keysInTree.size()));
+		if (maxNumberOfRebalancesInDelete > Math.log10(keysInTree.size()) / Math.log10(2)) {
+			System.err.println(
+					"log(" + keysInTree.size() + ")= " + df.format((Math.log10(keysInTree.size()) / Math.log10(2)))
+							+ "\nWorst case in delete is greater than log(n)!\nCheck your rebalance's methods!");
+		}
+		System.out.println("Number of rebalancing operations in all deletes: " + totalDeleteRebalances);
+		System.out.println("Worst Case rebalancing operations in single delete: " + maxNumberOfRebalancesInDelete);
+		System.out.println("Average rebalancing operations in delete: "
+				+ df.format((double) totalDeleteRebalances / keysInTree.size()));
+		System.out.println("********************************************************");
+		System.out.println();
+	}
+
+	public void runTest(int num, int max, int testNum) {
+		System.out.println("************Test " + testNum + "************");
+		treeGenerator(num, max);
+		if (num < 50)
+			printTree();
+		testSize();
+		testExistanceOfElements();
+		testKeysToArrayAndInfoToArray();
+		testMinAndMax();
+		treeDelete();
+		testEmpty();
+		printInfo();
+
+	}
+
+	public void run() {
+		runTest(20, 100, 0);
+		initialize();
+		for (int i = 1; i <= 10; i++) {
+			runTest(i * 10000, i * 500000, i);
+			initialize();
+		}
+	}
+
 }
