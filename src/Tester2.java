2ada7f838440c0719d8ae1ea611bcbb552012e7d
Ofir Feffer <ofir.feffer@gmail.com>
2016-12-20 22:18:42 +0200

    new tests

--- src/Tester2.java
+++ src/Tester2.java
@@ -5,119 +5,163 @@ import java.util.Random;
 
 public class Tester2 {
 
-    private static int height(WAVLTree.WAVLNode node) {
-        if (node == null) {
-            return -1;
-        }
-        return Math.max(height(node.getLeftChild()), height(node.getRightChild())) + 1;
-    }
+	static List<Integer> keysInTree = new ArrayList<>();
 
-    private static int getRank(WAVLTree.WAVLNode node) {
-        if (node == null) { // external leaf
-            return -1;
-        }
-        return node.getRank();
-    }
+	private static int height(WAVLTree.WAVLNode node) {
+		if (node == null) {
+			return -1;
+		}
+		return Math.max(height(node.getLeftChild()), height(node.getRightChild())) + 1;
+	}
 
-    private static void checkTreeAfterInsertions(WAVLTree.WAVLNode node) {
-        if (node == null) {
-            return;
-        }
-        if (height(node) != node.getRank()) {
-            System.out.println("problem with ranks. tree is not a valid AVL tree");
-            System.exit(1);
-        }
-        if (node.getLeftChild() != null && node.getLeftChild().getParent() != node) {
-            System.out.println("problem with parenting");
-            System.exit(1);
-        }
-        if (node.getRightChild() != null && node.getRightChild().getParent() != node) {
-            System.out.println("problem with parenting");
-            System.exit(1);
-        }
-        checkTreeAfterInsertions(node.getLeftChild());
-        checkTreeAfterInsertions(node.getRightChild());
-    }
+	private static int getRank(WAVLTree.WAVLNode node) {
+		if (node == null) { // external leaf
+			return -1;
+		}
+		return node.getRank();
+	}
 
-    private static void checkTreeAfterDeletions(WAVLTree.WAVLNode node) {
-        if (node == null) {
-            return;
-        }
-        if (node.getLeftChild() != null && node.getLeftChild().getParent() != node) {
-            System.out.println("problem with parenting");
-            System.exit(1);
-        }
-        if (node.getRightChild() != null && node.getRightChild().getParent() != node) {
-            System.out.println("problem with parenting");
-            System.exit(1);
-        }
-        int x = getRank(node) - getRank(node.getRightChild());
-        int y = getRank(node) - getRank(node.getLeftChild());
-        if (x>2 || x <1) {
-            System.out.println("Problem with ranks");
-            System.exit(1);
-        }
-        if (y>2 || y <1) {
-            System.out.println("Problem with ranks");
-            System.exit(1);
-        }
-        checkTreeAfterDeletions(node.getLeftChild());
-        checkTreeAfterDeletions(node.getRightChild());
-    }
+	private static void insertNonRandomElements(WAVLTree tree, int numOfElements) {
+		System.out.println("Inserting " + numOfElements + " elements");
+		for (int i = 1; i <= numOfElements; i++) {
+			;
+			tree.insert(i, String.valueOf(i));
+			keysInTree.add(i);
+		}
 
-    public static void main(String[] args) {
-        int numOfElements = 1_000_000;
+		System.out.println("Done insertion of " + numOfElements + " elements");
+	}
 
-        WAVLTree tree = new WAVLTree();
-        List<Integer> nums = new ArrayList<>();
-        Random rnd = new Random();
+	private static void insertRandomElements(WAVLTree tree, int numOfElements) {
+		Random rnd = new Random();
+		System.out.println("Inserting " + numOfElements + " elements");
 
-        System.out.println("Inserting "+ numOfElements + " elements");
+		for (int i = 1; i <= numOfElements; i++) {
+			int num = rnd.nextInt(Integer.MAX_VALUE);
+			tree.insert(num, String.valueOf(num));
+			keysInTree.add(num);
+		}
 
-        for (int i = 1; i <= numOfElements; i++) {
-            int num = rnd.nextInt(Integer.MAX_VALUE);
-            //int num = i;
-            tree.insert(num, String.valueOf(num));
-            nums.add(num);
-        }
+		System.out.println("Done insertion of " + numOfElements + " elements");
+	}
 
-        System.out.println("Done insertion of " + numOfElements + " elements");
-        System.out.println("Checking if tree is valid AVL tree");
-        checkTreeAfterInsertions(tree.getRoot());
-        System.out.println("Tree is valid");
-        int height = height(tree.getRoot());
-        System.out.printf("size: %d\theight: %d%n", tree.size(), height);
-        checkHeight(tree, height);
+	private static void deleteRandomKeys(WAVLTree tree) {
+		Collections.shuffle(keysInTree);
+		System.out.println("Deleting elements in random order");
+		for (int i = 0; i < keysInTree.size(); i++) {
+			tree.delete(keysInTree.get(i));
+			if (i % 10000 == 0) {
+				System.out.println("Checking ranks");
+				if (!checkTreeAfterDeletions(tree.getRoot())) {
+					System.out.println("Something went wrong with the ranks during deletion");
+					System.exit(1);
+				} else {
+					System.out.println("Tree is still valid");
+				}
+			}
+		}
+	}
 
-        Collections.shuffle(nums);
-        System.out.println("Now deleting elements in random order");
-        for (int i = 0; i < nums.size(); i++) {
-            tree.delete(nums.get(i));
-            if (i % 10000 == 0) {
-                System.out.println("Checking ranks");
-                checkTreeAfterDeletions(tree.getRoot());
-                System.out.println("Tree is still valid");
-            }
-        }
+	private static boolean checkTreeAfterInsertions(WAVLTree.WAVLNode node) {
+		if (node == null) {
+			return true;
+		} else if (height(node) != node.getRank()) {
+			return false;
+		}
+		int x = getRank(node) - getRank(node.getRightChild());
+		int y = getRank(node) - getRank(node.getLeftChild());
+		if (x > 2 || x < 1) {
+			return false;
+		}
+		if (y > 2 || y < 1) {
+			return false;
+		}
+		if (x == 2 && y == 2) {
+			return false;
+		}
 
-        if (tree.empty()) {
-            System.out.println("done. tree is empty");
-        } else {
-            System.out.println("WTF");
-        }
+		boolean res = checkTreeAfterInsertions(node.getLeftChild());
+		res = res && checkTreeAfterInsertions(node.getRightChild());
+		return res;
+	}
 
-        //Tester tester = new Tester();
-        //tester.run();
-    }
+	private static boolean checkTreeAfterDeletions(WAVLTree.WAVLNode node) {
+		if (node == null) {
+			return true;
+		}
+		int x = getRank(node) - getRank(node.getRightChild());
+		int y = getRank(node) - getRank(node.getLeftChild());
+		if (x > 2 || x < 1) {
+			return false;
+		}
+		if (y > 2 || y < 1) {
+			return false;
+		}
 
-    private static void checkHeight(WAVLTree tree, double height) {
-        if (height <= 1.45 * (Math.log10(tree.size()) / Math.log10(2))) {
-            System.out.println("height is <= 1.45*log(n)\n");
-        } else {
-            System.out.println("tree is not balanced.");
-            System.exit(0);
-        }
-    }
+		boolean res = checkTreeAfterDeletions(node.getLeftChild());
+		res = res && checkTreeAfterDeletions(node.getRightChild());
+		return res;
+	}
 
+	private static boolean checkHeight(WAVLTree tree) {
+		int height = height(tree.getRoot());
+		if (height <= 1.45 * (Math.log10(tree.size()) / Math.log10(2)))
+			return true;
+		return false;
+	}
+
+	private static boolean checkSearchMethod(WAVLTree tree) {
+		Collections.shuffle(keysInTree);
+		for (int key : keysInTree) {
+			String value = tree.search(key);
+			if (value == null || !(value.equals(String.valueOf(key)))) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		int numOfElements = 1_000_000;
+		WAVLTree tree = new WAVLTree();
+
+		System.out.println("**********************************************");
+		insertRandomElements(tree, numOfElements);
+		//insertNonRandomElements(tree, numOfElements);
+
+		System.out.println("Checking if all keys are in the tree");
+		if (!checkSearchMethod(tree)) {
+			System.out.println("Not all keys were inserted, or search function doesn't work");
+			System.exit(1);
+		} else {
+			System.out.println("All keys are in the tree");
+		}
+
+		System.out.println("Checking if tree is a valid AVL tree");
+		if (!checkTreeAfterInsertions(tree.getRoot())) {
+			System.out.println("Tree is NOT a valid AVL tree");
+			System.exit(1);
+		} else {
+			System.out.println("Tree is valid");
+		}
+
+		/*System.out.println("Checking if height is smaller than 1.45log(n)");
+		if (!checkHeight(tree)) {
+			System.out.println("Height of tree is not valid. tree is not balanced");
+		} else {
+			System.out.println("Height is valid");
+		}*/
+
+		System.out.println("**********************************************\n");
+
+		deleteRandomKeys(tree);
+
+		if (tree.empty()) {
+			System.out.println("done. tree is empty");
+		} else {
+			System.out.println("WTF: tree is marked as not empty yet we deleted all keys");
+		}
+	}
 
 }
